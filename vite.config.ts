f07744410aa2ee628a9a854e4d116ff9c37b8755
Daniel Glomberg <git@danielglomberg.com>
2023-05-08 00:03:19 +0200

    use externalizeDeps vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
 export default defineConfig({
   build: {
-    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'pcstoremodule',
       fileName: 'pcstoremodule',
     },
-    rollupOptions: {
-      external: ['zustand', 'mqtt'], // Add external dependencies here
-      output: {
-        globals: {
-          zustand: 'createStore', // Provide global variable name for Zustand
-          mqtt: 'mqtt', // Provide global variable name for mqtt.js
-        },
-      },
-    },
   },
-  plugins: [dts()],
+  plugins: [dts(), externalizeDeps()],
 })
