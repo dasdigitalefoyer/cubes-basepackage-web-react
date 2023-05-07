0c90f7240fdf1265cc322fed4de88009aa78be45
Daniel Glomberg <git@danielglomberg.com>
2023-05-07 17:28:24 +0200

    Implement mqtt connect and disconnect functions

--- src/services/MqttService.ts
+++ src/services/MqttService.ts
@@ -1,29 +1,77 @@
-export class MqttService {
+import * as mqtt from 'mqtt'
+
+export interface IMqttService {
+  connect(host: string, options: mqtt.IClientOptions): void
+  disconnect(): void
+  subscribe(topic: string): void
+  unsubscribe(topic: string): void
+  publish(topic: string, message: string): void
+  onMessage(topic: string, message: string): void
+}
+
+export class MqttService implements IMqttService {
+  private client: mqtt.MqttClient | null = null
+
+  /**
+   * Constructor
+   */
   constructor() {
-    console.log('MqttService constructor called')
+    this.connect('wss://broker.mirevi.team:9001/mqtt', {
+      clientId: 'cubeFrontend_' + Math.random().toString(16).substr(2, 8),
+      username: 'mirevi',
+      password: 'pastelquail546',
+      clean: false,
+      reconnectPeriod: 1000,
+      connectTimeout: 30000,
+      keepalive: 60,
+      resubscribe: true,
+    })
   }
 
-  public connect(): void {
-    console.log('Connected to broker')
+  /**
+   * Connects to the MQTT broker
+   *
+   * @param host
+   * @param options
+   * @returns void
+   */
+  connect(host: string, options: mqtt.IClientOptions): void {
+    this.client = mqtt.connect(host, options)
+    this.client.on('connect', () => {
+      this.subscribe('cube/#')
+    })
+    this.client.on('reconnect', () => {
+      console.log('MQTT client reconnecting')
+    })
+    this.client.on('close', () => {
+      console.log('MQTT client disconnected')
+    })
+    this.client.on('message', (topic, message) => {
+      this.onMessage(topic, message.toString() as string)
+    })
   }
 
-  public disconnect(): void {
-    console.log('Disconnected from broker')
+  /**
+   * Disconnects from the MQTT broker
+   *
+   * @returns void
+   */
+  disconnect(): void {
+    if (this.client) {
+      this.client.end()
+      this.client = null
+    }
   }
-
-  public subscribe(topic: string): void {
-    console.log('Subscribed to topic: ' + topic)
+  subscribe(topic: string): void {
+    console.log('MQTT subscribe', topic)
   }
-
-  public unsubscribe(topic: string): void {
-    console.log('Unsubscribed from topic: ' + topic)
+  unsubscribe(topic: string): void {
+    console.log('MQTT unsubscribe', topic)
   }
-
-  public publish(topic: string, message: string): void {
-    console.log('Published to topic: ' + topic + ' with message: ' + message)
+  publish(topic: string, message: string): void {
+    console.log('MQTT publish', topic, message)
   }
-
-  public onMessage(topic: string, message: string): void {
-    console.log('Received message on topic: ' + topic + ' with message: ' + message)
+  onMessage(topic: string, message: string): void {
+    console.log('MQTT message received', topic, message)
   }
 }
