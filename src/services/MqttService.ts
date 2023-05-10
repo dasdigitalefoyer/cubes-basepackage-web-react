b49869840dd07b53056850acc3bd1409f89c70b6
Daniel Glomberg <git@danielglomberg.com>
2023-05-10 10:16:02 +0200

    console.log to check errors

--- src/services/MqttService.ts
+++ src/services/MqttService.ts
@@ -1,18 +1,19 @@
 import * as mqtt from 'mqtt'
 import { CubeState } from '../models/CubeState'
-import { vanillaCubeStateStore, vanillaMqttStateStore } from '../stores'
+import { vanillaCubeStateStore } from '../stores'
 
 /**
  * MQTT service
  */
 export class MqttService {
   private static instance: MqttService
-  private client = vanillaMqttStateStore.getState().client
+  private client: mqtt.MqttClient | null = null
 
   /**
    * Constructor
    */
   private constructor() {
+    console.log('[MqttService] Initializing MQTT service')
     if (!this.client) {
       this.connect('wss://broker.mirevi.team:9001/mqtt', {
         clientId: 'cubeFrontend_' + Math.random().toString(16).substr(2, 8),
@@ -40,8 +41,9 @@ export class MqttService {
    */
   public connect(host: string, options: mqtt.IClientOptions): void {
     if (!this.client) {
+      console.log('[connect] Connecting to MQTT broker')
       this.client = mqtt.connect(host, options)
-      vanillaMqttStateStore.setState({ client: this.client })
+      //vanillaMqttStateStore.setState({ client: this.client })
       this.client.on('connect', () => {
         this.subscribeOnConnect()
       })
@@ -62,9 +64,10 @@ export class MqttService {
    */
   public disconnect(): void {
     if (this.client) {
+      console.log('[disconnect] Disconnecting from MQTT broker')
       this.client.end()
       this.client = null
-      vanillaMqttStateStore.setState({ client: null })
+      //vanillaMqttStateStore.setState({ client: null })
     }
   }
 
@@ -73,6 +76,7 @@ export class MqttService {
    */
   public subscribe(topic: string): void {
     if (this.client) {
+      console.log('[subscribe] Subscribing to topic: ' + topic)
       this.client.subscribe(topic)
     }
   }
@@ -82,6 +86,7 @@ export class MqttService {
    */
   public unsubscribe(topic: string): void {
     if (this.client) {
+      console.log('[unsubscribe] Unsubscribing from topic: ' + topic)
       this.client.unsubscribe(topic)
     }
   }
@@ -91,6 +96,7 @@ export class MqttService {
    */
   public publish(topic: string, message: string, options: mqtt.IClientPublishOptions): void {
     if (this.client) {
+      console.log('[publish] Publishing to topic: ' + topic)
       this.client.publish(topic, message, options)
     }
   }
@@ -100,6 +106,7 @@ export class MqttService {
    * from the MQTT broker
    */
   private onMessage(topic: string, message: string): void {
+    console.log('[onMessage] Received message from topic: ' + topic)
     if (topic.startsWith('puzzleCubes/') && topic.endsWith('/state')) {
       const cubeId = topic.split('/')[1]
       const cubeState = JSON.parse(message) as CubeState
@@ -118,6 +125,7 @@ export class MqttService {
    * Method is called after the connection to the MQTT broker is established
    */
   private subscribeOnConnect(): void {
+    console.log('[subscribeOnConnect] Subscribing to topics')
     if (this.client) {
       this.client.subscribe('puzzleCubes/+/state')
     }
@@ -127,6 +135,7 @@ export class MqttService {
    * Publishes a message to all cubes
    */
   public publishToAllCubes(message: string): void {
+    console.log('[publishToAllCubes] Publishing message to all cubes')
     this.publish('puzzleCubes/+/state', message, { qos: 1 })
   }
 
@@ -134,6 +143,7 @@ export class MqttService {
    * Publishes a message to a specific cube using the cubeId
    */
   public publishToCube(cubeId: string, message: string): void {
+    console.log('[publishToCube] Publishing message to cube: ' + cubeId)
     this.publish('puzzleCubes/' + cubeId + '/state', message, { qos: 1 })
   }
 }
