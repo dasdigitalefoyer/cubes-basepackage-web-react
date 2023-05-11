86ff2b1db843ff38233172b78dcbfbee0d093b01
Daniel Glomberg <git@danielglomberg.com>
2023-05-11 16:33:11 +0200

    Configure compiler for react

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
-import * as packageJson from './package.json'
 
 export default defineConfig({
   plugins: [dts(), nodePolyfills(), react()],
@@ -12,6 +11,17 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'pcstoremodule',
       fileName: 'pcstoremodule',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'zustand', 'mqtt'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          zustand: 'create',
+          mqtt: 'mqtt',
+        },
+      },
     },
   },
 })
