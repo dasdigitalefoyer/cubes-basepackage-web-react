fd354c75b3ead86826eb09011483cc556af5ef5e
Daniel Glomberg <git@danielglomberg.com>
2023-05-11 16:04:34 +0200

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,12 @@ import react from '@vitejs/plugin-react'
 import * as packageJson from './package.json'
 
 export default defineConfig({
+  plugins: [dts(), nodePolyfills(), react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'pcstoremodule',
       fileName: 'pcstoremodule',
     },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
-    },
   },
-  plugins: [dts(), nodePolyfills(), react()],
 })
