e3e1081a827d45e67ec2fa58d7f782ae4016113e
Daniel Glomberg <git@danielglomberg.com>
2023-08-23 17:52:08 +0200

    AppState as Generic Type

--- src/stores/CubeStateStore.tsx
+++ src/stores/CubeStateStore.tsx
@@ -1,8 +1,5 @@
 import { create } from 'zustand'
 
-/**
- * CubeState
- */
 export type CubeState = {
   timestamp: string
   meta: {
@@ -32,53 +29,17 @@ export type CubeState = {
   }
 }
 
-/**
- * CubeApp
- */
-enum AppCategory {
-  USER = 'USER',
-  ADMIN = 'ADMIN'
-}
-
-export type CubeApp = {
-  timestamp: Date
-  meta: {
-    version: string
-  }
-  id: string
-  category: AppCategory
-  name: string
-  description: string
-  version: string
-  rootPath: string
-  webResourcePath?: string
-  webRoot: string
-  clientAppRoot: string
-  clientAppType: string
-  active: boolean
-  miscData: object
-}
-
-/**
- * StateStore
- */
-type StateStore = {
+type CubeStateStore = {
   cubeState: CubeState[]
   addCubeState: (cubeState: CubeState) => void
   updateCubeState: (cubeState: CubeState) => void
   removeCubeState: (id: string) => void
   findCubeState: (id: string) => CubeState | undefined
   existsCubeState: (id: string) => boolean
-  cubeApp: CubeApp | null
-  setCubeApp: (cubeApp: CubeApp) => void
 }
 
-/**
- * useStateStore
- */
-export const useStateStore = create<StateStore>((set, get) => ({
+export const useCubeStateStore = create<CubeStateStore>((set, get) => ({
   cubeState: [],
-  cubeApp: null,
   addCubeState: (cubeState) => set((state) => ({ cubeState: [...state.cubeState, cubeState] })),
   updateCubeState: (cubeState) =>
     set((state) => ({ cubeState: state.cubeState.map((state) => (state.id === cubeState.id ? cubeState : state)) })),
@@ -90,6 +51,5 @@ export const useStateStore = create<StateStore>((set, get) => ({
   existsCubeState: (cubeId) => {
     const { cubeState } = get()
     return cubeState.some((state) => state.id === cubeId)
-  },
-  setCubeApp: (cubeApp) => set(() => ({ cubeApp }))
+  }
 }))
