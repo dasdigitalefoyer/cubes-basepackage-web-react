08f0e6001275e86d68800668444b6ca4cd25102e
Daniel Glomberg <git@danielglomberg.com>
2023-08-31 10:40:39 +0200

    Define AppStateBase

--- src/stores/AppStateStore.tsx
+++ src/stores/AppStateStore.tsx
@@ -1,14 +1,31 @@
 import { create } from 'zustand'
 
-export interface AppStateStore<T extends { cubeId: string }> {
+/**
+ * AppStateStore
+ *
+ * This store is used to store the state of the app/game.
+ * The state can vary from app to app and is defined by the generic type T.
+ * The generic type T must extend the AppStateBase interface to ensure that cubeid, isRunning and timestamp are available.
+ * The cubeId is used to identify the state of the app.
+ *
+ * * The store is not yet used in the app. It needs some research how it can be used.
+ */
+
+interface AppStateBase {
+  cubeId: string
+  isRunning: boolean
+  timestamp: string
+}
+
+export interface AppStateStore<T extends AppStateBase> {
   appState: T[]
   addAppState: (item: T) => void
   updateAppState: (item: T) => void
-  findAppState: (predicate: (item: T) => boolean) => T | undefined
+  findAppState: (cubeId: string) => T | undefined
   existsAppState: (cubeId: string) => boolean
 }
 
-export const useAppStateStore = <T extends { cubeId: string }>() =>
+export const useAppStateStore = <T extends AppStateBase>() =>
   create<AppStateStore<T>>()((set, get) => ({
     appState: [],
     addAppState: (item: T) => {
@@ -19,13 +36,13 @@ export const useAppStateStore = <T extends { cubeId: string }>() =>
         appState: state.appState.map((item) => (item.cubeId === updatedItem.cubeId ? updatedItem : item))
       }))
     },
-    findAppState: (predicate: (item: T) => boolean) => {
-      const currentState = get() // Get the current state
-      const foundItem = currentState.appState.find(predicate)
-      return foundItem !== undefined ? foundItem : undefined
+    findAppState: (cubeId: string) => {
+      const currentState = get()
+      const foundItem = currentState.appState.find((item) => item.cubeId === cubeId)
+      return foundItem
     },
     existsAppState: (cubeId: string) => {
-      const currentState = get() // Get the current state
+      const currentState = get()
       const foundItem = currentState.appState.find((item) => item.cubeId === cubeId)
       return foundItem !== undefined ? true : false
     }
